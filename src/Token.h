1b782b4b9bc8128b38ccbc88462bbeb066ed550f
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2020-06-25 22:08:35 -0600

    Update Token.h

--- src/Token.h
+++ src/Token.h
@@ -1,22 +1,35 @@
 #pragma once
 
+#include <string>
+
 enum class TokenTypes
 {
-	BOOL,
-	CHAR,
-	INT,
-	VARIABLE,
-	ASSIGNMENT,
-	INT_VALUE,
-	BOOL_VALUE,
-	CHAR_VALUE,
-	PLUS,
-	MINUS,
-	TIMES,
-	DIVIDE,
-	OPEN_BRACKET,
-	CLOSE_BRACKET,
-	SEMICOLON
+	BOOL_TYPE,      // 0
+	CHAR_TYPE,      // 1
+	INT_TYPE,       // 2
+
+	VARIABLE,       // 3
+
+	ASSIGNMENT,     // 4
+
+	PLUS,           // 5
+	MINUS,          // 6
+	TIMES,          // 7
+	DIVIDE,         // 8
+	MOD,            // 9
+
+	NOT,            // 10
+	OR,             // 11
+	AND,            // 12
+
+	OPEN_BRACKET,   // 13
+	CLOSE_BRACKET,  // 14
+
+	BOOL_VALUE,     // 15
+	CHAR_VALUE,     // 16
+	INT_VALUE,      // 17
+
+	SEMICOLON       // 18
 };
 
 struct Token
