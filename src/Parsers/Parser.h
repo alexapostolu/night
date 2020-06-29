eb23f2e6213b4b3aad6fd0bbac3015f6a8f75e13
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2020-06-29 14:13:19 -0600

    Update Parser.h

--- src/Parsers/Parser.h
+++ src/Parsers/Parser.h
@@ -1,138 +1,117 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 
 #include "Token.h"
 #include "Variable.h"
 
-#include "MathParser.h"
 #include "BoolParser.h"
+#include "IntParser.h"
+#include "StrParser.h"
 
-void ExtractVariableValue(const std::vector<Variable> variables, std::vector<Token>& tokens);
-void CheckExpression(const std::vector<Token>& tokens, TokenTypes valueType, int start, int end,
-	std::string error);
-bool CheckEquation(const std::vector<Token>& tokens, int index, TokenTypes type,
-	int start, int end);
+void CheckVariable(const std::vector<Variable>& vars, const Token& var)
+{
+	for (std::size_t a = 0; a < vars.size(); ++a)
+	{
+		if (vars[a].name == var.token)
+		{
+			std::cout << "Error - redefinition of variable '" << var.token << "'\n";
+			exit(0);
+		}
+	}
+}
 
 void Parser(std::vector<Token>& tokens)
 {
-	// make a singleton class that holds these values?
 	static std::vector<Variable> variables;
 
-	// variable initialization
-	if (tokens.size() >= 5 && tokens[1].type == TokenTypes::VARIABLE &&
-		tokens[2].type == TokenTypes::ASSIGNMENT)
+	if (tokens.size() == 3 && tokens[0].type == TokenType::PRINT)
 	{
-		ExtractVariableValue(variables, tokens);
-
-		if (tokens[0].type == TokenTypes::BOOL_TYPE)
+		if (tokens[1].type == TokenType::VARIABLE)
 		{
-			for (std::size_t a = 3; a < tokens.size() - 1; ++a)
+			bool definedVariable = false;
+			for (std::size_t b = 0; b < variables.size(); ++b)
 			{
-				if (tokens[a].type == TokenTypes::NOT &&
-					tokens[a + 1].type == TokenTypes::BOOL_VALUE)
+				if (tokens[1].token == variables[b].name)
 				{
-					tokens[a + 1].token = (tokens[a + 1].token == "true" ? "false" : "true");
-					tokens.erase(tokens.begin() + a);
-				}
-				else if (tokens[a].type == TokenTypes::NOT &&
-					tokens[a + 1].type == TokenTypes::OPEN_BRACKET)
-				{
-					int closeBracketIndex = -1;
-					for (std::size_t b = a + 1; b < tokens.size(); ++b)
-					{
-						if (tokens[b].type == TokenTypes::CLOSE_BRACKET)
-							closeBracketIndex = b;
-					}
+					std::cout << variables[b].value << '\n';
 
-					if (closeBracketIndex == -1)
-					{
-						std::cout << "Error - missing closing bracket";
-						exit(0);
-					}
-
-					for (std::size_t b = a + 1; b < tokens.size(); ++b)
-					{
-						if (tokens[b].type == TokenTypes::BOOL_VALUE)
-							tokens[b].token = (tokens[b].token == "true" ? "false" : "true");
-					}
-
-					tokens.erase(tokens.begin() + a);
+					definedVariable = true;
+					break;
 				}
 			}
 
-			CheckExpression(tokens, TokenTypes::BOOL_VALUE, 11, 12, "boolean");
-
-			std::vector<Token> temp(tokens.begin() + 3, tokens.end() - 1);
-			variables.push_back(Variable{ "bool", tokens[1].token, BoolParser(temp) });
-
-			std::cout << variables.back().type << ' ' << variables.back().name << ' ' <<
-				variables.back().value;
-		}
-		else if (tokens[0].type == TokenTypes::CHAR_TYPE)
-		{
-			// character types don't support arithmetic
-			if (tokens.size() > 5)
+			if (!definedVariable)
 			{
-				std::cout << "Error - invalid character expression";
+				std::cout << "Error - undefined variable '" << tokens[1].token << "'\n";
 				exit(0);
 			}
-			else
-			{
-				if (tokens[3].type == TokenTypes::CHAR_VALUE)
-				{
-					variables.push_back(Variable{ "char", tokens[1].token, tokens[3].token });
-				}
-				else
-				{
-					std::cout << "Error - invalid character assignment";
-					exit(0);
-				}
-			}
 		}
-		else if (tokens[0].type == TokenTypes::INT_TYPE)
+		else if (tokens[1].type == TokenType::BOOL_VALUE || tokens[1].type == TokenType::CHAR_VALUE ||
+			tokens[1].type == TokenType::INT_VALUE || tokens[1].type == TokenType::STRING_VALUE)
 		{
-			CheckExpression(tokens, TokenTypes::INT_VALUE, 5, 9, "integer");
-			
-			std::vector<Token> temp(tokens.begin() + 3, tokens.end() - 1);
-			variables.push_back(Variable{ "int", tokens[1].token, MathParser(temp) });
+			std::cout << tokens[1].token << '\n';
 		}
 		else
 		{
-			std::cout << "Error - invalid variable initialization";
+			std::cout << "Error - invalid print statement\n";
 			exit(0);
 		}
 	}
-	// variable assignment
-	else if (tokens.size() >= 4 && tokens[0].type == TokenTypes::VARIABLE &&
-		tokens[1].type == TokenTypes::ASSIGNMENT)
+	else if (tokens.size() >= 5)
 	{
-		// make sure variable is defined
-		bool definedVariable = false;
-		for (std::size_t a = 0; a < variables.size(); ++a)
+		if (tokens[0].type == TokenType::BOOL_TYPE && tokens[1].type == TokenType::VARIABLE &&
+			tokens[2].type == TokenType::ASSIGNMENT)
 		{
-			if (tokens[0].token == variables[a].name)
+			if (!CheckBool(tokens, variables))
 			{
-				definedVariable = true;
-				break;
+				std::cout << "Error - invalid boolean expression\n";
+				exit(0);
 			}
-		}
 
-		if (!definedVariable)
+			CheckVariable(variables, tokens[1]);
+
+			std::vector<Token> temp(tokens.begin() + 3, tokens.end() - 1);
+			variables.push_back(Variable{tokens[0].token , tokens[1].token, BoolParser(temp)});
+		}
+		else if (tokens[0].type == TokenType::CHAR_TYPE && tokens[1].type == TokenType::VARIABLE &&
+			tokens[2].type == TokenType::ASSIGNMENT && tokens[3].type == TokenType::CHAR_VALUE)
 		{
-			std::cout << "Error - variable not defined";
+			variables.push_back(Variable{ "char", tokens[1].token, tokens[3].token });
+		}
+		else if (tokens[0].type == TokenType::INT_TYPE && tokens[1].type == TokenType::VARIABLE &&
+			tokens[2].type == TokenType::ASSIGNMENT)
+		{
+			if (!CheckInt(tokens, variables))
+			{
+				std::cout << "Error - invalid integer expression\n";
+				exit(0);
+			}
+
+			CheckVariable(variables, tokens[1]);
+
+			std::vector<Token> temp(tokens.begin() + 3, tokens.end() - 1);
+			variables.push_back(Variable{ tokens[0].token , tokens[1].token, IntParser(temp) });
+		}
+		else if (tokens[0].type == TokenType::STRING_TYPE && tokens[1].type == TokenType::VARIABLE &&
+			tokens[2].type == TokenType::ASSIGNMENT)
+		{
+			if (!CheckStr(tokens, variables))
+			{
+				std::cout << "Error - invalid string expression\n";
+				exit(0);
+			}
+
+			CheckVariable(variables, tokens[1]);
+
+			std::vector<Token> temp(tokens.begin() + 1, tokens.end() - 1);
+			variables.push_back(Variable{ tokens[0].token , tokens[1].token, StrParser(temp) });
+		}
+		else
+		{
+			std::cout << "Error - invalid variable initialization\n";
 			exit(0);
-		}
-
-		ExtractVariableValue(variables, tokens);
-	}
-	else if (tokens.size() >= 3)
-	{
-		ExtractVariableValue(variables, tokens);
-		
-		for (std::size_t a = 1; a < tokens.size() - 1; ++a)
-		{
-			std::cout << tokens[a].token;
 		}
 	}
 	else
@@ -140,149 +119,4 @@ void Parser(std::vector<Token>& tokens)
 		std::cout << "Error - invalid language grammar\n";
 		exit(0);
 	}
-}
-
-// turn variables into their types
-void ExtractVariableValue(const std::vector<Variable> variables, std::vector<Token>& tokens)
-{
-	for (std::size_t a = 2; a < tokens.size() - 1; ++a)
-	{
-		if (tokens[a].type != TokenTypes::VARIABLE)
-			continue;
-
-		bool definedVariable = false;
-		for (std::size_t b = 0; b < variables.size(); ++b)
-		{
-			if (tokens[a].token == variables[a].name)
-			{
-				tokens[a].type = (variables[a].type == "bool" ?
-					TokenTypes::BOOL_TYPE : (variables[a].type == "char" ?
-						TokenTypes::CHAR_TYPE : TokenTypes::INT_TYPE));
-
-				tokens[a].token = variables[a].value;
-
-				break;
-			}
-		}
-
-		if (!definedVariable)
-		{
-			std::cout << "Error - variable not defined";
-			exit(0);
-		}
-	}
-}
-
-void CheckExpression(const std::vector<Token>& tokens, TokenTypes type, int start, int end, 
-	std::string error)
-{
-	int openBrackets = 0;
-	for (std::size_t a = 3; a < tokens.size() - 1; ++a)
-	{
-		if (!CheckEquation(tokens, a, type, start, end))
-		{
-			std::cout << "Error - invalid " << error << " expression";
-			exit(0);
-		}
-
-		if (tokens[a].type == TokenTypes::OPEN_BRACKET)
-			openBrackets += 1;
-		else if (tokens[a].type == TokenTypes::CLOSE_BRACKET)
-			openBrackets -= 1;
-	}
-
-	if (openBrackets > 0)
-	{
-		std::cout << "Error - missing closing bracket";
-		exit(0);
-	}
-	else if (openBrackets < 0)
-	{
-		std::cout << "Error - missing opening bracket";
-		exit(0);
-	}
-}
-
-bool CheckEquation(const std::vector<Token>& tokens, int index, TokenTypes valueType, int start, int end)
-{
-	// checks all numbers
-	if (tokens[index].type == valueType)
-	{
-		if ((tokens[index - 1].type != TokenTypes::ASSIGNMENT ||
-				tokens[index + 1].type != TokenTypes::SEMICOLON) &&
-			(tokens[index - 1].type != TokenTypes::ASSIGNMENT ||
-				(int)tokens[index + 1].type < start || (int)tokens[index + 1].type > end) &&
-			((int)tokens[index - 1].type < start || (int)tokens[index - 1].type > end ||
-				(int)tokens[index + 1].type < start || (int)tokens[index + 1].type > end) &&
-			(tokens[index - 1].type != TokenTypes::OPEN_BRACKET ||
-				(int)tokens[index + 1].type < start || (int)tokens[index + 1].type > end) &&
-			((int)tokens[index - 1].type < start || (int)tokens[index - 1].type > end ||
-				tokens[index + 1].type != TokenTypes::CLOSE_BRACKET) &&
-			(tokens[index - 1].type != TokenTypes::OPEN_BRACKET ||
-				tokens[index + 1].type != TokenTypes::CLOSE_BRACKET) &&
-			((int)tokens[index - 1].type < start || (int)tokens[index - 1].type > end ||
-				(tokens[index + 1].type != TokenTypes::SEMICOLON)))
-		{
-			return false;
-		}
-	}
-	// checks all operators
-	else if ((int)tokens[index].type >= start && (int)tokens[index].type <= end)
-	{
-		if ((tokens[index - 1].type != valueType ||
-				tokens[index + 1].type != valueType) &&
-			(tokens[index - 1].type != valueType ||
-				tokens[index + 1].type != TokenTypes::OPEN_BRACKET) &&
-			(tokens[index - 1].type != TokenTypes::CLOSE_BRACKET ||
-				tokens[index + 1].type != valueType) &&
-			(tokens[index - 1].type != TokenTypes::CLOSE_BRACKET ||
-				tokens[index + 1].type != TokenTypes::OPEN_BRACKET))
-		{
-			return false;
-		}
-	}
-	// checks all open brackets
-	else if (tokens[index].type == TokenTypes::OPEN_BRACKET)
-	{
-		if (((int)tokens[index - 1].type < start || (int)tokens[index - 1].type > end ||
-				tokens[index + 1].type != valueType) &&
-			((int)tokens[index - 1].type < start || (int)tokens[index - 1].type > end ||
-				tokens[index + 1].type != TokenTypes::OPEN_BRACKET) &&
-			(tokens[index - 1].type != TokenTypes::OPEN_BRACKET ||
-				tokens[index + 1].type != valueType) &&
-			(tokens[index - 1].type != TokenTypes::OPEN_BRACKET ||
-				tokens[index + 1].type != TokenTypes::OPEN_BRACKET) &&
-			(tokens[index - 1].type != TokenTypes::ASSIGNMENT ||
-				tokens[index + 1].type != TokenTypes::OPEN_BRACKET) &&
-			(tokens[index - 1].type != TokenTypes::ASSIGNMENT ||
-				tokens[index + 1].type != valueType))
-		{
-			return false;
-		}
-	}
-	// checks all close brackets
-	else if (tokens[index].type == TokenTypes::CLOSE_BRACKET)
-	{
-		if ((tokens[index - 1].type != valueType ||
-				(int)tokens[index + 1].type < start || (int)tokens[index + 1].type > end) &&
-			(tokens[index - 1].type != TokenTypes::CLOSE_BRACKET ||
-				(int)tokens[index + 1].type < start || (int)tokens[index + 1].type > end) &&
-			(tokens[index - 1].type != valueType ||
-				tokens[index + 1].type != TokenTypes::CLOSE_BRACKET) &&
-			(tokens[index - 1].type != TokenTypes::CLOSE_BRACKET ||
-				tokens[index + 1].type != TokenTypes::CLOSE_BRACKET) &&
-			(tokens[index - 1].type != valueType ||
-				tokens[index + 1].type != TokenTypes::SEMICOLON) &&
-			(tokens[index - 1].type != TokenTypes::CLOSE_BRACKET ||
-				tokens[index + 1].type != TokenTypes::SEMICOLON))
-		{
-			return false;
-		}
-	}
-	else
-	{
-		return false;
-	}
-
-	return true;
 }
