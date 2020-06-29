bf9fcf8ccb0f78771687188ce3ef6ec10fac40be
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2020-06-29 14:07:55 -0600

    Create Token.h

---
+++ Token.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+
+enum class TokenType
+{
+	BOOL_TYPE,
+	CHAR_TYPE,
+	INT_TYPE,
+	STRING_TYPE,
+
+	VARIABLE,
+
+	ASSIGNMENT,
+
+	BOOL_VALUE,
+	CHAR_VALUE,
+	INT_VALUE,
+	STRING_VALUE,
+
+	PLUS,
+	MINUS,
+	TIMES,
+	DIVIDE,
+	MOD,
+
+	NOT,
+	OR,
+	AND,
+
+	OPEN_BRACKET,
+	CLOSE_BRACKET,
+
+	// casts?
+
+	PRINT,
+
+	SEMICOLON
+};
+
+struct Token
+{
+	TokenType type;
+	std::string token;
+};
