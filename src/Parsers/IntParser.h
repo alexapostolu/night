158e432c0f59a82365e301d6c7985ee94af319f7
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2020-07-07 12:33:50 -0600

    Update IntParser.h

--- src/Parsers/IntParser.h
+++ src/Parsers/IntParser.h
@@ -7,132 +7,27 @@
 #include "Error.h"
 #include "Token.h"
 
-void CheckInt(int start, std::vector<Token>& expr, const std::vector<Variable>& vars, TokenType ss = TokenType::ASSIGNMENT)
-{
-	/* valid integer expressions:
-	 = 5 +          5 + 5          = ( 5          5 ) +
-	 = 5 ;          5 + (          = ( (          5 ) )
-	 + 5 )          ) + 5          + ( 5          5 ) ;
-	 + 5 +          ) + (          + ( (          ) ) +
-	 + 5 ;                         ( ( 5          ) ) )
-	 ( 5 +                         ( ( (          ) ) ;
-	 ( 5 )
-	 */
-
-	int openBracket = 0;
-	for (std::size_t a = start; a < expr.size() - 1; ++a)
-	{
-		if (expr[a].type == TokenType::OPEN_BRACKET)
-			openBracket += 1;
-		else if (expr[a].type == TokenType::CLOSE_BRACKET)
-			openBracket -= 1;
-
-		// checks value
-		if (expr[a].type == TokenType::INT_VALUE)
-		{
-			if ((expr[a - 1].type != ss ||
-					expr[a + 1].type < TokenType::PLUS || expr[a + 1].type > TokenType::MOD) &&
-				(expr[a - 1].type != ss ||
-					expr[a + 1].type != TokenType::SEMICOLON)
-				&&
-				(expr[a - 1].type < TokenType::PLUS || expr[a - 1].type > TokenType::MOD ||
-					expr[a + 1].type != TokenType::CLOSE_BRACKET) &&
-				(expr[a - 1].type < TokenType::PLUS || expr[a - 1].type > TokenType::MOD ||
-					expr[a + 1].type < TokenType::PLUS || expr[a + 1].type > TokenType::MOD) &&
-				(expr[a - 1].type < TokenType::PLUS || expr[a - 1].type > TokenType::MOD ||
-					expr[a + 1].type != TokenType::SEMICOLON)
-				&&
-				(expr[a - 1].type != TokenType::OPEN_BRACKET ||
-					expr[a + 1].type < TokenType::PLUS || expr[a + 1].type > TokenType::MOD) &&
-				(expr[a - 1].type != TokenType::OPEN_BRACKET ||
-					expr[a + 1].type != TokenType::CLOSE_BRACKET))
-			{
-				error("1invalid integer expression");
-			}
-		}
-		// checks operator
-		else if (expr[a].type >= TokenType::PLUS && expr[a].type <= TokenType::MOD)
-		{
-			if ((expr[a - 1].type != TokenType::INT_VALUE ||
-					expr[a + 1].type != TokenType::INT_VALUE) &&
-				(expr[a - 1].type != TokenType::INT_VALUE ||
-					expr[a + 1].type != TokenType::OPEN_BRACKET)
-				&&
-				(expr[a - 1].type != TokenType::CLOSE_BRACKET ||
-					expr[a + 1].type != TokenType::INT_VALUE) &&
-				(expr[a - 1].type != TokenType::CLOSE_BRACKET ||
-					expr[a + 1].type != TokenType::OPEN_BRACKET))
-			{
-				error("2invalid integer expression");
-			}
-		}
-		// checks open bracket
-		else if (expr[a].type == TokenType::OPEN_BRACKET)
-		{
-			if ((expr[a - 1].type != ss ||
-					expr[a + 1].type != TokenType::INT_VALUE) &&
-				(expr[a - 1].type != ss ||
-					expr[a + 1].type != TokenType::OPEN_BRACKET)
-				&&
-				(expr[a].type < TokenType::PLUS || expr[a].type > TokenType::MOD ||
-					expr[a + 1].type != TokenType::INT_VALUE) &&
-				(expr[a].type < TokenType::PLUS || expr[a].type > TokenType::MOD ||
-					expr[a + 1].type != TokenType::OPEN_BRACKET)
-				&&
-				(expr[a - 1].type != TokenType::OPEN_BRACKET ||
-					expr[a + 1].type != TokenType::INT_VALUE) &&
-				(expr[a - 1].type != TokenType::OPEN_BRACKET ||
-					expr[a + 1].type != TokenType::OPEN_BRACKET))
-			{
-				error("3invalid integer expression");
-			}
-		}
-		// checks close bracket
-		else if (expr[a].type == TokenType::CLOSE_BRACKET)
-		{
-			if ((expr[a - 1].type != TokenType::INT_VALUE ||
-					expr[a + 1].type < TokenType::PLUS || expr[a + 1].type > TokenType::MOD) &&
-				(expr[a - 1].type != TokenType::INT_VALUE ||
-					expr[a + 1].type != TokenType::CLOSE_BRACKET) &&
-				(expr[a - 1].type != TokenType::INT_VALUE ||
-					expr[a + 1].type != TokenType::SEMICOLON)
-				&&
-				(expr[a - 1].type != TokenType::CLOSE_BRACKET ||
-					expr[a + 1].type < TokenType::PLUS || expr[a + 1].type > TokenType::MOD) &&
-				(expr[a - 1].type != TokenType::CLOSE_BRACKET ||
-					expr[a + 1].type != TokenType::CLOSE_BRACKET) &&
-				(expr[a - 1].type != TokenType::CLOSE_BRACKET ||
-					expr[a + 1].type != TokenType::SEMICOLON))
-			{
-				error("4invalid integer expression");
-			}
-		}
-		else
-		{
-			error("5invalid integer expression");
-		}
-	}
-
-	if (openBracket > 0)
-		error("closing bracket not found");
-	else if (openBracket < 0)
-		error("opening bracket not found");
-}
-
 void EvaluateIntExpression(std::vector<std::string>& expr, std::size_t& index)
 {
-	if (expr[index] == "+")
-		expr[index - 1] = std::to_string(stoi(expr[index - 1]) + stoi(expr[index + 1]));
-	else if (expr[index] == "-")
-		expr[index - 1] = std::to_string(stoi(expr[index - 1]) - stoi(expr[index + 1]));
-	else if (expr[index] == "*")
-		expr[index - 1] = std::to_string(stoi(expr[index - 1]) * stoi(expr[index + 1]));
-	else if (expr[index] == "/")
-		expr[index - 1] = std::to_string(stoi(expr[index - 1]) / stoi(expr[index + 1]));
-	else if (expr[index] == "%")
-		expr[index - 1] = std::to_string(stoi(expr[index - 1]) % stoi(expr[index + 1]));
-	else
-		error("Did you forget to add an operator?");
+	try
+	{
+		if (expr[index] == "+")
+			expr.at(index - 1) = std::to_string(stoi(expr.at(index - 1)) + stoi(expr.at(index + 1)));
+		else if (expr[index] == "-")
+			expr.at(index - 1) = std::to_string(stoi(expr.at(index - 1)) - stoi(expr.at(index + 1)));
+		else if (expr[index] == "*")
+			expr.at(index - 1) = std::to_string(stoi(expr.at(index - 1)) * stoi(expr.at(index + 1)));
+		else if (expr[index] == "/")
+			expr.at(index - 1) = std::to_string(stoi(expr.at(index - 1)) / stoi(expr.at(index + 1)));
+		else if (expr[index] == "%")
+			expr.at(index - 1) = std::to_string(stoi(expr.at(index - 1)) % stoi(expr.at(index + 1)));
+		else
+			error("Did you forget to add an operator?");
+	}
+	catch (...)
+	{
+		error("invalid integer expression");
+	}
 
 	expr.erase(expr.begin() + index);
 	expr.erase(expr.begin() + index);
@@ -214,6 +109,9 @@ std::string IntParser(const std::vector<Token>& tokens)
 		else if (expr[a] == "-")
 			EvaluateIntExpression(expr, a);
 	}
+
+	if (expr.size() > 1)
+		error("invalid integer expression");
 
 	return expr[0];
 }
