34ede1fcd0630f0325a6136d95101b8887c232f8
DynamicSquid <apostolu120@gmail.com>
2020-11-01 11:43:07 -0700

    v4

--- src/lexer.h
+++ src/lexer.h
@@ -1,331 +1,112 @@
 #pragma once
 
-#include "lib/string.h"
-#include "lib/array.h"
-#include "lib/error.h"
+#include "token.h"
 
-#include "containers/token.h"
+#include <regex>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
-bool is_letter(char c)
+void FindKeyword(std::unordered_map<std::string, TokenType>& keywords, std::vector<Token>& tokens, std::string& token,
+	const std::string& file, int line)
 {
-	return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
-}
+	auto findKeyword = keywords.find(token);
+	if (findKeyword != keywords.end())
+		tokens.push_back(Token{ file, line, findKeyword->second, token });
+	else if (std::regex_match(token, std::regex("[0-9]+")))
+		tokens.push_back(Token{ file, line, TokenType::NUM_VAL, token });
+	else if (std::regex_match(token, std::regex("((\\+|-)?([0-9]+)(\\.[0-9]+)?)|((\\+|-)?\\.?[0-9]+)")))
+		tokens.push_back(Token{ file, line, TokenType::NUM_VAL, token });
+	else if (std::regex_match(token, std::regex("[a-zA-Z_][a-zA-Z_0-9]*")))
+		tokens.push_back(Token{ file, line, TokenType::VARIABLE, token });
+	else
+		return;
 
-bool match_character(night::string& token)
-{
-	if (token.length() == 4 && token[1] == '\\' && token[2] == 'n')
-	{
-		token = '\n';
-		return true;
-	}
-
-	if (token.length() != 3)
-		return false;
-
-	if (token[0] == '\'' && token[2] == '\'')
-	{
-		token = token[1];
-		return true;
-	}
-
-	return false;
-}
-
-int match_number(const night::string& token)
-{
-	int decimalCount = 0;
-	for (int a = 0; a < token.length(); ++a)
-	{
-		if ((token[a] - '0' < 0 || token[a] - '0' > 9) && token[a] != '.')
-			return -1;
-
-		if (token[a] == '.' && ++decimalCount > 1)
-			return -1;
-	}
-
-	return decimalCount == 1;
-}
-
-bool match_variable(const night::string& token)
-{
-	if (token[0] != '_' && !is_letter(token[0]))
-		return false;
-
-	for (int a = 1; a < token.length(); ++a)
-	{
-		if (token[a] != '_' && !is_letter(token[a]) && (token[a] - '0' < 0 || token[a] - '0' > 9))
-			return false;
-	}
-
-	return true;
-}
-
-void AddKeyword(night::array<Token>& tokens, night::string& token, TokenType&& type)
-{
-	tokens.add_back(Token{ type, token });
 	token = "";
 }
 
-void CheckToken(night::array<Token>& tokens, night::string& token)
+std::vector<Token> Lexer(const std::string& file, int line, const std::string& fileLine)
 {
-	if (token.length() == 0)
-		return;
+	std::unordered_map<std::string, TokenType> symbols{
+		{ "||", TokenType::OPERATOR },
+		{ "&&", TokenType::OPERATOR },
+		{ "(", TokenType::OPEN_BRACKET },
+		{ ")", TokenType::CLOSE_BRACKET },
+		{ "[", TokenType::OPEN_SQUARE },
+		{ "]", TokenType::CLOSE_SQUARE },
+		{ "{", TokenType::OPEN_CURLY },
+		{ "}", TokenType::CLOSE_CURLY },
+		{ ",", TokenType::COMMA }
+	};
 
-	if (token == "bit")
-		AddKeyword(tokens, token, TokenType::BIT_TYPE);
-	else if (token == "syb")
-		AddKeyword(tokens, token, TokenType::SYB_TYPE);
-	else if (token == "int")
-		AddKeyword(tokens, token, TokenType::INT_TYPE);
-	else if (token == "dec")
-		AddKeyword(tokens, token, TokenType::DEC_TYPE);
-	else if (token == "str")
-		AddKeyword(tokens, token, TokenType::STR_TYPE);
-	else if (token == "null")
-		AddKeyword(tokens, token, TokenType::NULL_TYPE);
-	else if (token == "true" || token == "false")
-		AddKeyword(tokens, token, TokenType::BIT_VALUE);
-	else if (token == "if")
-		AddKeyword(tokens, token, TokenType::IF);
-	else if (token == "else")
-		AddKeyword(tokens, token, TokenType::ELSE);
-	else if (token == "return")
-		AddKeyword(tokens, token, TokenType::RETURN);
-	else if (token == "loop")
-		AddKeyword(tokens, token, TokenType::LOOP);
-	else if (token == "while")
-		AddKeyword(tokens, token, TokenType::WHILE);
-	else if (token == "for")
-		AddKeyword(tokens, token, TokenType::FOR);
-	else if (token == "import" || token == "include")
-		AddKeyword(tokens, token, TokenType::IMPORT);
-	else if (match_character(token))
-		AddKeyword(tokens, token, TokenType::SYB_VALUE);
-	else if (match_number(token) == 0)
-		AddKeyword(tokens, token, TokenType::INT_VALUE);
-	else if (match_number(token) == 1)
-		AddKeyword(tokens, token, TokenType::DEC_VALUE);
-	else if (match_variable(token))
-		AddKeyword(tokens, token, TokenType::VARIABLE);
-}
+	std::unordered_map<char, std::pair<TokenType, TokenType> > doubleSymbols{
+		{ '>', { TokenType::OPERATOR, TokenType::OPERATOR } },
+		{ '<', { TokenType::OPERATOR, TokenType::OPERATOR } },
+		{ '!', { TokenType::OPERATOR, TokenType::OPERATOR } },
+		{ '+', { TokenType::OPERATOR, TokenType::ASSIGNMENT } },
+		{ '-', { TokenType::OPERATOR, TokenType::ASSIGNMENT } },
+		{ '*', { TokenType::OPERATOR, TokenType::ASSIGNMENT } },
+		{ '/', { TokenType::OPERATOR, TokenType::ASSIGNMENT } },
+		{ '%', { TokenType::OPERATOR, TokenType::ASSIGNMENT } },
+		{ '=', { TokenType::ASSIGNMENT, TokenType::ASSIGNMENT } }
+	};
 
-void AddSymbol(night::array<Token>& tokens, night::string& token, char symbol, TokenType&& type)
-{
-	CheckToken(tokens, token);
-	tokens.add_back(Token{ type, symbol });
-}
+	std::unordered_map<std::string, TokenType> keywords{
+		{ "true", TokenType::BOOL_VAL },
+		{ "false", TokenType::BOOL_VAL },
+		{ "set", TokenType::SET },
+		{ "if", TokenType::IF },
+		{ "else", TokenType::ELSE },
+		{ "while", TokenType::WHILE },
+		{ "for", TokenType::FOR },
+		{ "import", TokenType::IMPORT },
+		{ "include", TokenType::IMPORT },
+	};
 
-night::array<Token> Lexer(const night::string& line)
-{
-	night::string token;
-	night::array<Token> tokens;
-
-	bool isString = false;
-	for (int a = 0; a < line.length(); ++a)
+	std::vector<Token> tokens;
+	std::string token = "";
+	for (std::size_t a = 0; a < fileLine.length(); ++a)
 	{
-		if (a < line.length() - 1 && line[a] == '/' && line[a + 1] == '/')
-			break;
-
-		if (line[a] == '"' && !isString)
+		auto findSymbol = symbols.find(std::string(1, fileLine[a]));
+		if (findSymbol != symbols.end())
 		{
-			CheckToken(tokens, token);
+			FindKeyword(keywords, tokens, token, file, line);
 
-			isString = true;
-			continue;
-		}
-		else if (line[a] != '"' && isString)
-		{
-			token += line[a] == '\t' ? ' ' : line[a];
-			continue;
-		}
-		else if (line[a] == '"' && isString)
-		{
-			AddKeyword(tokens, token, TokenType::STR_VALUE);
+			tokens.push_back(Token{ file, line, findSymbol->second, token });
 
-			isString = false;
 			continue;
 		}
 
-		switch (line[a])
+		auto findDoubleSymbol = std::find_if(doubleSymbols.begin(), doubleSymbols.end(),
+			[&](const std::pair<char, std::pair<TokenType, TokenType> >& symbol) { return symbol.first == fileLine[a]; });
+		if (findDoubleSymbol != doubleSymbols.end())
 		{
-		case '=':
-			CheckToken(tokens, token);
-			if (a < line.length() - 1 && line[a + 1] == '=')
+			FindKeyword(keywords, tokens, token, file, line);
+
+			if (a < fileLine.length() - 1 && fileLine[a + 1] == '=')
 			{
-				tokens.add_back(Token{ TokenType::EQUALS, "==" });
-				a++;
+				tokens.push_back(Token{
+					file, line,
+					findDoubleSymbol->second.second,
+					findDoubleSymbol->first + "="
+				});
 			}
 			else
 			{
-				tokens.add_back(Token{ TokenType::ASSIGNMENT, "=" });
+				tokens.push_back(Token{
+					file, line,
+					findDoubleSymbol->second.first,
+					std::string(1, findDoubleSymbol->first)
+				});
 			}
 
-			break;
-		case '+':
-			CheckToken(tokens, token);
-			if (a < line.length() - 1 && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::PLUS_ASSIGN, "+=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::PLUS, "+" });
-			}
-
-			break;
-		case '-':
-			CheckToken(tokens, token);
-			if (a < line.length() - 1 && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::MINUS_ASSIGN, "-=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::MINUS, "-" });
-			}
-
-			break;
-		case '*':
-			CheckToken(tokens, token);
-			if (a < line.length() - 1 && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::TIMES_ASSIGN, "*=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::TIMES, "*" });
-			}
-
-			break;
-		case '/':
-			CheckToken(tokens, token);
-			if (a < line.length() - 1 && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::DIVIDE_ASSIGN, "/=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::DIVIDE, "/" });
-			}
-
-			break;
-		case '%':
-			CheckToken(tokens, token);
-			if (a < line.length() - 1 && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::MOD_ASSIGN, "%=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::MOD, "%" });
-			}
-
-			break;
-		case '!':
-			CheckToken(tokens, token);
-			if (a < line.length() - 1 && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::NOT_EQUALS, "!=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::NOT, "!" });
-			}
-
-			break;
-		case '|':
-			if (a < line.length() - 1 && line[a + 1] == '|')
-			{
-				CheckToken(tokens, token);
-				tokens.add_back(Token{ TokenType::OR, "||" });
-
-				a++;
-			}
-
-			break;
-		case '&':
-			if (a < line.length() - 1 && line[a + 1] == '&')
-			{
-				CheckToken(tokens, token);
-				tokens.add_back(Token{ TokenType::AND, "&&" });
-
-				a++;
-			}
-
-			break;
-		case '>':
-			CheckToken(tokens, token);
-			if (a < line.length() && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::GREATER_EQUAL, "<=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::GREATER, ">" });
-			}
-
-			break;
-		case '<':
-			CheckToken(tokens, token);
-			if (a < line.length() && line[a + 1] == '=')
-			{
-				tokens.add_back(Token{ TokenType::SMALLER_EQUAL, "<=" });
-				a++;
-			}
-			else
-			{
-				tokens.add_back(Token{ TokenType::SMALLER, "<" });
-			}
-
-			break;
-		case '(':
-			AddSymbol(tokens, token, line[a], TokenType::OPEN_BRACKET);
-			break;
-		case ')':
-			AddSymbol(tokens, token, line[a], TokenType::CLOSE_BRACKET);
-			break;
-		case '[':
-			AddSymbol(tokens, token, line[a], TokenType::OPEN_SQUARE);
-			break;
-		case ']':
-			AddSymbol(tokens, token, line[a], TokenType::CLOSE_SQUARE);
-			break;
-		case '{':
-			AddSymbol(tokens, token, line[a], TokenType::OPEN_CURLY);
-			break;
-		case '}':
-			AddSymbol(tokens, token, line[a], TokenType::CLOSE_CURLY);
-			break;
-		case ',':
-			AddSymbol(tokens, token, line[a], TokenType::COMMA);
-			break;
-		case ';':
-			AddSymbol(tokens, token, line[a], TokenType::SEMICOLON);
-			break;
-		case ' ':
-			CheckToken(tokens, token);
-			break;
-		case '\n':
-			CheckToken(tokens, token);
-			break;
-		case '\t':
-			CheckToken(tokens, token);
-			break;
-		default:
-			token += line[a];
+			continue;
 		}
+
+		token += fileLine[a];
 	}
 
-	CheckToken(tokens, token);
-	if (token != "")
-		throw Error(night::_invalid_token_, tokens, tokens.length() - 1, tokens.length() - 1, "token "_s + token + "' is not a valid token"_s);
-
+	tokens.push_back(Token{ "", 0, TokenType::EOL });
 	return tokens;
 }
--- src/night.cpp
+++ src/night.cpp
@@ -1,20 +1,323 @@
-#include <stdio.h>
+//#define _CRTDBG_MAP_ALLOC
+//#include <stdlib.h>
+//#include <crtdbg.h>
 
-#include "lib/error.h"
-#include "./file_extraction.h"
+#include "lexer.h"
+#include "parser.h"
+#include "interpreter.h"
+#include "error.h"
+#include "token.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void OpenFile(const std::string& file)
+{
+    std::ifstream sourceFile(file);
+    if (!sourceFile.is_open())
+        throw Error("source file could not be opened");
+
+    std::vector<Token> tokens;
+    std::string fileLine;
+    for (int line = 0; getline(sourceFile, fileLine); ++line)
+    {
+        std::vector<Token> fileTokens = Lexer(file, line, fileLine);
+        if (fileTokens.size() >= 1 && fileTokens[0].type == TokenType::IMPORT)
+        {
+            if (fileTokens.size() == 1 || fileTokens[1].type != TokenType::STRING_VAL)
+                throw Error(file, line, "expected file name after '" + fileTokens[0].value + "' statement");
+            if (fileTokens.size() > 2)
+                throw Error(file, line, "import statement must be on it's own line");
+
+            std::ifstream importFile((fileTokens[0].value == "import" ? "../dusk_pkgs/" : "") + fileTokens[1].value);
+            if (!importFile.is_open())
+                throw Error(file, line, "file '" + fileTokens[1].value + "' cannot be opened");
+
+            std::vector<Token> importTokens;
+            for (int lineNumber2 = 0; getline(importFile, fileLine); ++lineNumber2)
+            {
+                std::vector<Token> importTemp = Lexer(fileTokens[1].value, lineNumber2, fileLine);
+                importTokens.insert(importTokens.end(), importTemp.begin(), importTemp.end());
+            }
+
+            importFile.close();
+
+            fileTokens.erase(fileTokens.begin());
+            fileTokens.erase(fileTokens.begin());
+
+            fileTokens.insert(fileTokens.begin(), importTokens.begin(), importTokens.end());
+        }
+
+        fileTokens.push_back(Token{ file, line, TokenType::EOL });
+        tokens.insert(tokens.end(), fileTokens.begin(), fileTokens.end());
+    }
+
+    sourceFile.close();
+
+    std::vector<Statement*> statements;
+    std::vector<std::vector<Token> > code = SplitCode(tokens);
+    for (const std::vector<Token>& tokens : code)
+        Parser(statements, tokens);
+
+    Interpreter(statements);
+}
 
 int main(int argc, char* argv[])
 {
     try {
         if (argc > 2)
-            throw "invalid command line arguments";
-
-        ExtractFile(argc == 1 ? "source.night" : argv[1]);
-    }
-    catch (const char* e) {
-        fprintf(stderr, "[error] - %s\n", e);
+            throw Error("invalid command line arguments");
+        
+        OpenFile(argc == 1 ? "C:\\dev\\solutions\\Night\\Night v4\\Night\\source.night" : argv[1]);
     }
     catch (const Error& e) {
-        fprintf(stderr, "%s\n", e.what().cstr());
+        std::cerr << e.what() << '\n';
+    }
+
+    //_CrtDumpMemoryLeaks();
+}
+
+/*
+
+class AST
+{
+private:
+    struct Node;
+
+public:
+    static Token EvaluateExpression(const std::vector<Token>& expression)
+    {
+        Node* head = CreateTree(expression);
+        TravelTree(head);
+
+        Token result = head->token;
+        delete head;
+
+        return result;
+    }
+
+public:
+    static Node* CreateTree(const std::vector<Token>& expression)
+    {
+        if (expression.size() == 1)
+            return new Node{ expression[0], nullptr, nullptr };
+
+        Node* head = nullptr;
+        for (std::size_t a = 0; a < expression.size(); ++a)
+        {
+            if (expression[a].type == TokenType::PLUS)
+            {
+                if (expression[a + 1].type == TokenType::OPEN_BRACKET)
+                {
+                    head = new Node{
+                        expression[a],
+                        head == nullptr ? new Node{ expression[a - 1], nullptr, nullptr } : head,
+                        CreateTree(GetBracketExpression(expression, a))
+                    };
+
+                    continue;
+                }
+
+                head = new Node{
+                    expression[a],
+                    head == nullptr ? new Node{ expression[a - 1], nullptr, nullptr } : head,
+                    new Node{ expression[a + 1], nullptr, nullptr }
+                };
+            }
+            else if (expression[a].value == "*")
+            {
+                if (head == nullptr)
+                {
+                    if (expression[a + 1].value == "(")
+                    {
+                        head = new Node{
+                            expression[a],
+                            new Node{ expression[a - 1], nullptr, nullptr },
+                            CreateTree(GetBracketExpression(expression, a))
+                        };
+
+                        continue;
+                    }
+
+                    head = new Node{
+                        expression[a],
+                        new Node{ expression[a - 1], nullptr, nullptr },
+                        new Node{ expression[a + 1], nullptr, nullptr }
+                    };
+
+                    continue;
+                }
+
+                if (expression[a + 1].value == "(")
+                {
+                    head->right = new Node{
+                        expression[a],
+                        head->right,
+                        CreateTree(GetBracketExpression(expression, a))
+                    };
+
+                    continue;
+                }
+
+                head->right = new Node{
+                    expression[a],
+                    head->right,
+                    new Node{ expression[a + 1], nullptr, nullptr }
+                };
+            }
+        }
+
+        return head;
+    }
+
+    static void TravelTree(Node* node)
+    {
+        if (node->left == nullptr || node->right == nullptr)
+            return;
+
+        TravelTree(node->left);
+        TravelTree(node->right);
+
+        if (node->left->token.type == "val" && node->right->token.type == "val")
+        {
+            if (node->token.value == "+")
+                node->token = Token{ "val", std::to_string(std::stoi(node->left->token.value) + std::stoi(node->right->token.value)) };
+            else if (node->token.value == "*")
+                node->token = Token{ "val", std::to_string(std::stoi(node->left->token.value) * std::stoi(node->right->token.value)) };
+
+            delete node->left;
+            delete node->right;
+        }
+    }
+
+private:
+    static std::vector<Token> GetBracketExpression(const std::vector<Token>& expression, std::size_t& index)
+    {
+        index += 2;
+
+        std::vector<Token> bracket_expression;
+        for (int count = 0; index < expression.size(); ++index)
+        {
+            if (expression[index].value == "(")
+                count++;
+            else if (expression[index].value == ")")
+                count--;
+
+            if (expression[index].value == ")" && count == -1)
+                return bracket_expression;
+
+            bracket_expression.push_back(expression[index]);
+        }
+    }
+
+private:
+    struct Node
+    {
+        Token token;
+        Node* left;
+        Node* right;
+    };
+
+    Node* head;
+};
+
+*/
+
+/*
+
+
+std::vector<Value> GetBracketExpression(const std::vector<Value>& expression, int& index)
+{
+    index += 2;
+
+    std::vector<Value> bracket_expression;
+    for (int count = 0; index < expression.size(); ++index)
+    {
+        if (expression[index].type == ValueType::OPEN_BRACKET)
+            count++;
+        else if (expression[index].type == ValueType::CLOSE_BRACKET)
+            count--;
+
+        if (expression[index].type == ValueType::CLOSE_BRACKET && count == -1)
+            return bracket_expression;
+
+        bracket_expression.push_back(expression[index]);
     }
 }
+
+Expression* ParseExpression(const std::vector<Value>& expression)
+{
+    if (expression.size() == 1)
+        return new Expression{ expression[0], nullptr, nullptr };
+
+    Expression* head = nullptr;
+    for (int a = 0; a < expression.size(); ++a)
+    {
+        if (GetOperatorPrecedence(expression[a]) == 1)
+        {
+            if (head == nullptr)
+            {
+                if (expression[a + 1].type == ValueType::OPEN_BRACKET)
+                {
+                    head = new Expression{
+                        expression[a],
+                        new Expression{ expression[a - 1], nullptr, nullptr },
+                        ParseExpression(GetBracketExpression(expression, a))
+                    };
+
+                    continue;
+                }
+
+                head = new Expression{
+                    expression[a],
+                    new Expression{ expression[a - 1], nullptr, nullptr },
+                    new Expression{ expression[a + 1], nullptr, nullptr }
+                };
+
+                continue;
+            }
+
+            if (expression[a + 1].type == ValueType::OPEN_BRACKET)
+            {
+                head->right = new Expression{
+                    expression[a],
+                    head->right,
+                    ParseExpression(GetBracketExpression(expression, a))
+                };
+
+                continue;
+            }
+
+            head->right = new Expression{
+                expression[a],
+                head->right,
+                new Expression{ expression[a + 1], nullptr, nullptr }
+            };
+        }
+        else if (GetOperatorPrecedence(expression[a]) == 2)
+        {
+            if (expression[a + 1].type == ValueType::OPEN_BRACKET)
+            {
+                head = new Expression{
+                    expression[a],
+                    head == nullptr ? new Expression{ expression[a - 1], nullptr, nullptr } : head,
+                    ParseExpression(GetBracketExpression(expression, a))
+                };
+
+                continue;
+            }
+
+            head = new Expression{
+                expression[a],
+                head == nullptr ? new Expression{ expression[a - 1], nullptr, nullptr } : head,
+                new Expression{ expression[a + 1], nullptr, nullptr }
+            };
+        }
+    }
+
+    return head;
+}
+
+*/
