7f1a6efc7c6e55ee35312c12a37dcb5d04c5fc63
AntimatterReactor <ezraalvarion71@gmail.com>
2021-10-25 20:06:28 +0700

    Change const strings to const char[]
    Note: general global variables use const,
    while error class color variables use constexpr

--- code/include/error.hpp
+++ code/include/error.hpp
@@ -15,31 +15,31 @@ struct Location
 
 namespace night {
 
-std::string const format_array = "arrays must be in this format: `[elem1, elem2]`";
-std::string const format_subscript = "subscript operators must be in this format: `array[index]`";
-std::string const format_init = "variable initializations must be in this format: `let variable = expression`";
-std::string const format_if = "if conditionals must be in this format: `if (condition) {}`";
-std::string const format_elif = "elif conditionals must be in this format: `elif (condition) {}`";
-std::string const format_else = "else conditionals must be in this format: `else {}`";
-std::string const format_while = "while loops must be in this format: `while (condition) {}`";
-std::string const format_for = "for loops must be in this format: `for (iterator : range) {}`";
-std::string const format_fn = "function definitions must be in this format: `fn function(parameters) {}`";
-std::string const format_call = "function calls must be in this format: `function(parameters)`";
-std::string const format_method = "method calls must be in this format: `object.method(parameters)`";
+const char format_array[] = "arrays must be in this format: `[elem1, elem2]`";
+const char format_subscript[] = "subscript operators must be in this format: `array[index]`";
+const char format_init[] = "variable initializations must be in this format: `let variable = expression`";
+const char format_if[] = "if conditionals must be in this format: `if (condition) {}`";
+const char format_elif[] = "elif conditionals must be in this format: `elif (condition) {}`";
+const char format_else[] = "else conditionals must be in this format: `else {}`";
+const char format_while[] = "while loops must be in this format: `while (condition) {}`";
+const char format_for[] = "for loops must be in this format: `for (iterator : range) {}`";
+const char format_fn[] = "function definitions must be in this format: `fn function(parameters) {}`";
+const char format_call[] = "function calls must be in this format: `function(parameters)`";
+const char format_method[] = "method calls must be in this format: `object.method(parameters)`";
 
-std::string const error_preprocess = "preprocessor";
-std::string const error_compile = "compile";
-std::string const error_runtime = "runtime";
+const char error_preprocess[] = "preprocessor";
+const char error_compile[] = "compile";
+const char error_runtime[] = "runtime";
 
 class error
 {
 public:
 	error(
 		std::string_view debug_file,
-		int const        debug_line,
+		int const debug_line,
 
 		std::string const& _type,
-		Location	const& _loc,
+		Location const& _loc,
 
 		std::string const& _msg,
 		std::string const& _fix
@@ -58,17 +58,17 @@ private:
 	std::string fix;
 
 private:
-	const std::string RESET = "\033[0m";
-	const std::string RED = "\033[0;31m";
-	const std::string YELLOW = "\033[0;33m";
-	const std::string CYAN = "\033[38;5;45m";
-	const std::string WHITE = "\033[0;37m";
-	const std::string b_RED = "\033[0;1;31m";
-	const std::string b_WHITE = "\033[0;1;37m";
-	const std::string u_RED = "\033[0;4;31m";
-	const std::string u_WHITE = "\033[0;4;37m";
-	const std::string bu_RED = "\033[0;1;4;31m";
-	const std::string bu_WHITE = "\033[0;1;4;37m";
+	static constexpr char RESET[] = "\033[0m";
+	static constexpr char RED[] = "\033[0;31m";
+	static constexpr char YELLOW[] = "\033[0;33m";
+	static constexpr char CYAN[] = "\033[38;5;45m";
+	static constexpr char WHITE[] = "\033[0;37m";
+	static constexpr char b_RED[] = "\033[0;1;31m";
+	static constexpr char b_WHITE[] = "\033[0;1;37m";
+	static constexpr char u_RED[] = "\033[0;4;31m";
+	static constexpr char u_WHITE[] = "\033[0;4;37m";
+	static constexpr char bu_RED[] = "\033[0;1;4;31m";
+	static constexpr char bu_WHITE[] = "\033[0;1;4;37m";
 };
 
 struct error_wrapper
