1a521a4325b529b6826b53050a62d4e25b28047f
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2022-03-26 20:00:48 -0600

    formatting, remove `noexcept`

--- code/include/back-end/token.hpp
+++ code/include/back-end/token.hpp
@@ -43,15 +43,17 @@ struct Token
 	TokenType type;
 	std::string data;
 
-	constexpr bool is_value() const noexcept
+	constexpr bool is_value() const
 	{
 		return type == TokenType::BOOL_L  || type == TokenType::INT_L ||
 			   type == TokenType::FLOAT_L || type == TokenType::STR_L ||
 			   type == TokenType::VAR     || type == TokenType::OPEN_SQUARE;
 	}
 
-	constexpr bool is_operator() const noexcept
-	{ return type == TokenType::UNARY_OP || type == TokenType::BINARY_OP; }
+	constexpr bool is_operator() const
+	{
+		return type == TokenType::UNARY_OP || type == TokenType::BINARY_OP;
+	}
 
 	static Token const _EOL;
 	static Token const _EOF;
